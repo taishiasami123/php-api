f1ceaedf42705d84fa84c46ed3357f5dfe7013bf
taishiasami123 <taishi.asami123@gmail.com>
2020-03-25 16:30:28 +0900

    json受け取ってバラした

--- signin.php
+++ signin.php
@@ -8,4 +8,13 @@
   // dbに繋ぐ
   require('dbconnect.php');
 
+  // jsonを取得
+  $json = file_get_contents("php://input");
+  $params = json_decode($json, true)['sign_in_user_params'];
+  $email = $params['email'];
+  $pwd = $params['password'];
+  $pwd = hash('sha256', $pwd);  // ハッシュ化
+  $pwdCfm = $params['password_confirmation'];
+  $pwdCfm = hash('sha256', $pwdCfm); //ハッシュ化
+
 ?>
