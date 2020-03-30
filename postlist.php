b0a8c6d172513a1394f25e1db4b5ebc52c6e07cf
taishiasami123 <taishi.asami123@gmail.com>
2020-03-30 14:58:19 +0900

    jsonを取得してバラす

--- postlist.php
+++ postlist.php
@@ -8,4 +8,13 @@
   // dbに繋ぐ
   require('dbconnect.php');
 
+  // jsonを取得
+  $header = getallheaders();
+  $bearerToken = $header['Authorization'];
+  $token = substr($bearerToken, 7, strlen($bearerToken) - 7);
+  $json = file_get_contents("php://input");
+  $page = $_GET['page'];
+  $limit = $_GET['limit'];
+  $keyword = $_GET['query'];
+
 ?>
